809c1a0d7a8b7e9d8dad7170c848a9e69207a474
apoorvgupta25 <apurvagupta45@gmail.com>
2025-10-24 16:10:33 +0530

    Added data module

---
+++ data/datastore/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
+}
+
+kotlin {
+
+    androidLibrary {
+        namespace = "com.apoorvgupta.datastore"
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "datastoreKit"
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(projects.core)
+
+            // Koin
+            implementation(libs.koin.core)
+
+            // Datastore
+            implementation(libs.datastore.preferences.core)
+        }
+    }
+}
